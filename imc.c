ab3ffa8dffd5923c655da1f6d1577a261ff054ca
João Guilherme <joaoguilherme@Laptop-de-Joao.local>
2026-05-22 20:17:21 -0300

    correção de ; e formula do IMC ajustada

--- imc.c
+++ imc.c
@@ -5,10 +5,10 @@ int main()
 	double peso, altura, imc;
 	printf("Bem-vindo à calculadora de IMC!\n");
 	printf("Digite seu peso (em kg): ");
-	scanf("%lf", &peso)
+	scanf("%lf", &peso);
 	printf("Digite sua altura (em metros): ");
 	scanf("%lf", &altura);
-	imc = peso / (pow(altura, 3));
+	imc = peso / (pow(altura,2));
 	printf("Seu IMC é: %.2lf\n", imc);
 	return 0;
 }
