067b01e74e9dcc63570ff7c338077ae8c642dab6
Bruno <brutoledobispo@gmail.com>
2026-05-22 18:36:24 -0300

    crie código que calcula imc

---
+++ imc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double peso, altura, imc;
+	printf("Bem-vindo à calculadora de IMC!\n");
+	printf("Digite seu peso (em kg): ");
+	scanf("%lf", &peso)
+	printf("Digite sua altura (em metros): ");
+	scanf("%lf", &altura);
+	imc = peso / (pow(altura, 3));
+	printf("Seu IMC é: %.2lf\n", imc);
+	return 0;
+}
+
